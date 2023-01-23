99d8df1eda7cc26d9a7c412126d8a0b744e720ac
Celibistrial <ryan80222@gmail.com>
2023-01-23 20:47:22 +0530

    added files

---
+++ polybar/forest/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/forest"
+~/scripts/walpaper
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar -q main -c "$DIR"/config.ini &
+
