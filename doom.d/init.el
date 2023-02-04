6cfa7cc410432e98e3de1276d762bd63f4d93e9a
Celibistrial <82810795+Celibistrial@users.noreply.github.com>
2023-02-04 16:23:29 +0530

    update configs

--- doom.d/init.el
+++ doom.d/init.el
@@ -24,7 +24,7 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       ;;ivy               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
@@ -99,7 +99,7 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -228,3 +228,4 @@
     (insert "}\n")
     (cd project-dir)
     (shell-command "cmake -H. -Bbuild")))
+
