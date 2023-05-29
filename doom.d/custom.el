8bae0dbb332cf6aec94a2542e78c79d3594c0f61
Celibistrial <ryan80222@gmail.com>
2023-05-29 19:58:19 +0530

    changed doom

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-types '((org-element-cache) (defvaralias))))
+ '(org-agenda-files
+   '("/home/gaurav/org/journal.org" "/home/gaurav/org/link-youtube.org" "/home/gaurav/org/refile.org"))
+ '(warning-suppress-types '((org-element-cache) (defvaralias)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-code-face ((t (:inherit default)))))
